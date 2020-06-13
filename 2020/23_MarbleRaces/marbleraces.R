f15b4ce78b6b8c80845aa10fe095cea205947501
Ariadna <ariadna.angulo@uab.cat>
2020-06-13 12:14:50 +0200

    week 23 and 24  tidytuesday

---
+++ 2020/23_MarbleRaces/marbleraces.R
@@ -0,0 +1,55 @@
+# 23	2020-06-02	Marble Races	Jelle's Marble Runs	Randy Olson
+
+
+library(extrafont) # la primera vez ejecutar font_import()
+loadfonts()
+font<- "Courier New" #Fuente que voy a utlizar
+
+# Get the Data
+
+marbles <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-06-02/marbles.csv')
+
+# Or read in with tidytuesdayR package (https://github.com/thebioengineer/tidytuesdayR)
+
+library(tidyverse)
+
+
+pole<-marbles<-marbles%>%
+filter(race %in%paste0("S1Q", 1:8))
+
+
+
+points<-marbles<-marbles%>%
+  filter(race %in%paste0("S1R", 1:8))
+
+points%>%
+  filter(points >= 5)%>%
+  group_by(team_name)%>%
+  count()
+
+paleta<-c("#F19754","#4380B9","#237B68","#4c6085","#39a0ed","#36f1cd","#13c4a3","#32322c",
+"#e9d758","#ff8552","#efbdeb","#b33f62","#f9564f","#8E8542","#96443E","#9E6B84")
+
+points%>%
+  filter(points >= 5)%>%
+  mutate(metersecond=time_s/(track_length_m *number_laps))%>%
+  ggplot(aes(x=metersecond, y=points,  color=team_name, label=team_name))+
+  geom_text(size=3, vjust="inward",hjust="inward", family=font, fontface= 2)+
+  facet_wrap(~race, nrow=2, scales = "free")+theme_bw()+
+  scale_color_manual(values=paleta)+
+  theme_minimal()+
+  theme(legend.position = "none", 
+        strip.background = element_rect(fill="#EF7A85"), 
+        text=element_text(family = font), 
+        plot.title = element_text(size=20, hjust=0,face="bold", color="#EF7A85"), 
+        plot.subtitle = element_text(face="italic", size=15, hjust=0), 
+        plot.caption = element_text( face="italic", size=12, hjust = 1))+
+  labs(x="m/s", 
+       caption="Done by @AnguloBrunet \n #tidytuesday", 
+       title= "Marble Racing", 
+       subtitle= "Points and speed by race")
+ggsave("marbles.png")
+
+
+
+
---
+++ 2020/24_africanamerican/africanamerican.R
@@ -0,0 +1,53 @@
+#24	2020-06-09	African-American Achievements	Wikipedia & Wikipedia	David Blackwell & Petition for David 
+
+
+color1 <-"#474350" #background color
+color2<- "#FA7268"  #color  countries
+
+theme_ari<- function(){
+  theme_bw()+
+
+    theme(
+      text=element_text( color="white"),
+      plot.background = element_rect(fill = color1, color=NA),
+          panel.background = element_blank(), 
+          panel.grid.major = element_blank(), 
+          panel.grid.minor = element_blank(),
+      axis.text.x = element_text(color="white"),
+      axis.text.y = element_text(color="white"),
+      plot.title = element_text(size=20, hjust=0,face="bold", color="#fadd9e"), 
+      plot.subtitle = element_text(face="italic", size=15, hjust=0, color="#f45b69"), 
+      plot.caption = element_text( face="italic", size=12, hjust = 1,color= "#f45b69"), 
+          legend.background = element_rect(fill=color1,
+                                           size=0.5, linetype="solid"), 
+          legend.title = element_text(colour="white", size=10, 
+                                      face="bold"), 
+          axis.line=element_blank(), 
+          legend.position = "bottom")
+    
+  }
+}
+# Get the Data
+
+firsts <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-06-09/firsts.csv')
+science <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-06-09/science.csv')
+
+
+firsts<-firsts%>%
+  filter(gender =="Female African American Firsts")
+
+paleta<- c("#cda2ab","#fadd9e","#6b9ac4","#97d8c4","#f4b942","#f45b69","#dc6acf","#efc3e6")
+
+firsts%>%
+  group_by(year, category)%>%
+  count()%>%
+  ggplot(aes(x=year, y=n,   fill=category))+
+  geom_col(position="stack", stat="identity")+theme_ari()+
+  scale_fill_manual(values=paleta)+
+  scale_y_continuous(breaks= c(1, 2, 3, 4, 5, 6))+
+  labs(fill="", 
+       subtitle = "Female achivements by category", 
+       title= "African-american achievements", 
+       caption= "Done by @AnguloBrunet \n #tidytuesday")
+  
+ggsave("africanamerican.png")
